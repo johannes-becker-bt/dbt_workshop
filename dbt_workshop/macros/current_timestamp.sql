6556b8481272c422d53a30fa80529ad01bddd174
Hannes <becker@rabgi.de>
2022-08-19 18:38:02 +0200

    use debt_utils, just to show it

--- dbt_workshop/macros/current_timestamp.sql
+++ dbt_workshop/macros/current_timestamp.sql
@@ -1,3 +1,4 @@
 {% macro sqlite__current_timestamp() %}
+{# nicht einmal notwendig, da die Standardimplementierung von dbt-core datetime() nutzt, was sqlite auch kann #}
     CURRENT_TIMESTAMP
 {% endmacro %}
--- dbt_workshop/macros/solutions.sql
+++ dbt_workshop/macros/solutions.sql
@@ -3,7 +3,5 @@
 Einfaches concat
 Die Lösung wäre in echt nicht empfehlenswert, da zB Die Spalten 1,11 und 11,1 das gleiche Ergebnis liefern würden.
 #}
-{%- for field in fields %}
-"{{field}}"{{ "||" if not loop.last }}
-{%- endfor %}
+{{dbt_utils.concat(fields)}}
 {% endmacro %}
