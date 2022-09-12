8ec785ddac145b5c7e52c118816077f9c6dcb074
Johannes Becker b.t <johannes.becker@btelligent.com>
2022-09-12 22:26:27 +0200

    add snapshot

---
+++ dbt_workshop/snapshots/BONUS_snap_penguins.sql
@@ -0,0 +1,20 @@
+/*
+Diskutiere mit deinem Nachbarn, welche Tabelle du bevorzugst:
+- A2_2_PENGUIN_SATELLITE
+- snap_penguins
+*/
+{% snapshot BONUS_snap_penguins %}
+
+{{
+    config(
+      target_database='main',
+      target_schema='main',
+      unique_key='business_key',
+      strategy='timestamp',
+      updated_at='insert_ts'
+    )
+}}
+
+select * from {{ ref('A1_2_INSERT_PENGUINS')}}
+
+{% endsnapshot %}
